c6fbdc1a6d4de930ae9a8dd686573bc6e554d1a4
Milos <116572641+milos-automation@users.noreply.github.com>
2023-05-23 14:26:30 +1000

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,14 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/signs', (req, res) => {
+  res.json({
+    "sign 1": {
+      "name": "100 km/h",
+      "qty": 0,
+      "url": "https://www.uags.com.au/assets/full/CF36AS.jpg?20200707030635"
+    }
+  })
+})
+
 app.listen(3000);
